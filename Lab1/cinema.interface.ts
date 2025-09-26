21be5bfc5edb53dd3029e2656d8650f7d8cf52d2
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-09-26 19:47:24 +0300

    feat: add TypeScript interfaces and enums

---
+++ Lab1/cinema.interface.ts
@@ -0,0 +1,78 @@
+type UUID = string;
+type DateTime = string;
+
+enum SeatType {
+  STANDARD = "STANDARD",
+  VIP = "VIP",
+}
+
+enum BookingStatus {
+  RESERVED = "RESERVED",
+  PAID = "PAID",
+  CANCELLED = "CANCELLED",
+}
+
+interface Movie {
+  movie_id: UUID;
+  title: string;
+  age_limit: number;
+  duration_min: number;
+  release_year: number;
+  description: string;
+}
+
+interface Genre {
+  genre_id: UUID;
+  name: string;
+}
+
+interface MovieGenre {
+  movie_id: UUID; // FK to Movie
+  genre_id: UUID; // FK to Genre
+}
+
+interface CinemaHall {
+  cinema_hall_id: UUID;
+  hall_number: number;
+  capacity: number;
+}
+
+interface Seat {
+  seat_id: UUID;
+  cinema_hall_id: UUID; // FK to CinemaHall
+  row: number;
+  number: number;
+  seatType: SeatType;
+}
+
+interface Showtime {
+  showtime_id: UUID;
+  movie_id: UUID; // FK to Movie
+  cinema_hall_id: UUID; // FK to CinemaHall
+  start_time: DateTime;
+}
+
+interface Customer {
+  customer_id: UUID;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+}
+
+interface Booking {
+  booking_id: UUID;
+  customer_id: UUID; // FK to Customer
+  showtime_id: UUID; // FK to Showtime
+  total_price: number;
+  status: BookingStatus;
+  booking_date: DateTime;
+}
+
+interface BookingSeat {
+  booking_seat_id: UUID;
+  booking_id: UUID; // FK to Booking
+  seat_id: UUID; // FK to Seat
+  showtime_id: UUID; // FK to Showtime
+  price: number;
+}
